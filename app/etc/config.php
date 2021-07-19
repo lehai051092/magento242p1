53678eeec0423524c32b0dcc8c41d1cefdf770a6
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-07-19 20:06:42 +0700

    create module

--- app/etc/config.php
+++ app/etc/config.php
@@ -314,6 +314,7 @@ return [
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
         'Dotdigitalgroup_Sms' => 1,
+        'Internships_Jobs' => 1,
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
         'Klarna_Kp' => 1,
